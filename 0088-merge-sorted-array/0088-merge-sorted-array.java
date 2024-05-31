4f7da497b37bba36b7df4aef467d00ef32493c8a
RITESH RAI <147366894+RITESH-RAI@users.noreply.github.com>
2024-05-31 11:27:39 +0530

    Time: 1 ms (33.91%), Space: 42.4 MB (12.14%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -8,13 +8,14 @@ class Solution {
     }
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        
+        // Copy nums2 into nums1 from index m onwards
         for (int i = 0; i < n; i++) {
             nums1[m + i] = nums2[i];
         }
 
+        // Total length of the combined array
         int len = m + n;
-       
+        // Initialize the gap
         int gap = (len / 2) + (len % 2);
         
         while (gap > 0) {
@@ -22,19 +23,20 @@ class Solution {
             int right = gap;
             while (right < len) {
                 if (left < m && right < m) {
-                    
+                    // Both elements in nums1
                     swap(nums1, left, nums1, right);
                 } else if (left < m && right >= m) {
-                   
+                    // Left element in nums1 and right element in nums2
                     swap(nums1, left, nums1, right);
                 } else {
-                    
+                    // Both elements in nums2
                     swap(nums1, left, nums1, right);
                 }
                 left++;
                 right++;
             }
 
+            // Reduce the gap
             if (gap == 1)
                 break;
             gap = (gap / 2) + (gap % 2);
