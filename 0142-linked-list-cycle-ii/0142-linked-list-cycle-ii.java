a4e790a33d1f9bf5d5c6dc7b75a986f22fd548b8
RITESH RAI <147366894+RITESH-RAI@users.noreply.github.com>
2024-07-07 21:40:52 +0530

    Time: 1 ms (28.21%), Space: 44.3 MB (51.26%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -18,9 +18,6 @@ public class Solution {
             slow=slow.next;
             fast=fast.next.next;
             if(slow==fast){
-                
-            
-        
         slow=head;
         while(slow!=fast){
             slow=slow.next;
