7802824a8ff4c21313f2eb393451139efff39aff
RITESH RAI <147366894+RITESH-RAI@users.noreply.github.com>
2024-07-28 11:05:44 +0530

    Time: 1 ms (99.78%), Space: 45 MB (22.55%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,58 +1,53 @@
 class Solution {
-    List<List<String>> ans= new ArrayList<>();
-    public List<List<String>> solveNQueens(int n) {
-      char board[][]= new char[n][n];
-        for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            board[i][j]='.';
+   public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
         }
-        }
-        nqueens(board,0);
-        return ans;
+
+        List<List<String>> res = new ArrayList<>();
+        int[] leftRow = new int[n];
+        int[] upperDiag = new int[2 * n - 1];
+        int[] lowerDiag = new int[2 * n - 1];
+
+        solve(0, board, res, leftRow, upperDiag, lowerDiag);
+        return res;
     }
-    public void nqueens(char board[][],int row){
-        if(row==board.length){
-            printboard(board);
-            return ;
+
+    private void solve(int col, char[][] board, List<List<String>> res, int[] leftRow, int[] upperDiag, int[] lowerDiag) {
+        int n = board.length;
+        if (col == n) {
+            res.add(construct(board));
+            return;
         }
-        for(int j=0;j<board.length;j++){
-            if(issafe(board,row,j)){
-                board[row][j]='Q';
-                nqueens(board,row+1);
-                board[row][j]='.';
+
+        for (int row = 0; row < n; row++) {
+            if (leftRow[row] == 0 && lowerDiag[row + col] == 0 && upperDiag[n - 1 + col - row] == 0) {
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                lowerDiag[row + col] = 1;
+                upperDiag[n - 1 + col - row] = 1;
+
+                solve(col + 1, board, res, leftRow, upperDiag, lowerDiag);
+
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiag[row + col] = 0;
+                upperDiag[n - 1 + col - row] = 0;
             }
         }
     }
-    public boolean issafe(char board[][],int row,int col){
-        for(int i=row-1;i>=0;i--){
-            if(board[i][col]=='Q'){
-            return false;
-            }
+
+    private static List<String> construct(char[][] board) {
+        List<String> res = new LinkedList<>();
+        for (int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            res.add(s);
         }
-        for(int i=row-1,j=col-1;i>=0 &&j>=0;i--,j--){
-            if(board[i][j]=='Q'){
-            return false;
-            }
-        }
-        for(int i=row-1,j=col+1;i>=0 &&j<board.length;i--,j++){
-             if(board[i][j]=='Q'){
-            return false;
-            }
-        }
-        return true;
+        return res;
     }
-    public void printboard(char board[][]){
-        List<String> ls= new ArrayList<>();
-        for(int i=0;i<board.length;i++){
-    String str="";
-     for(int j=0;j<board.length;j++)   {
-         str+=board[i][j];
-         
-     }    
-            ls.add(str);
-}
-        ans.add(ls);
-        
-}
-    
+
+
 }
